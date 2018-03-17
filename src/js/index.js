e7494b60239255495f8a8ab07e225b7c6259f824
Nic <lordtardus@gmail.com>
2018-03-17 13:34:41 +0100

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+
+
+
+function main() {
+    let entry1 = new LogEntry("65", "I am65 the Master65 of every65 little thing65");
+    console.log(entry1.dataNoKey);
+}
+
+
+$.ready(main());
---
+++ src/js/logentry.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Nic on 2018-03-17.
+ */
+
+
+class LogEntry {
+    constructor(key, data){
+        this.key = key;
+        this.data = data;
+    }
+
+    get dataNoKey(){
+        return this.removeKey();
+    }
+
+    removeKey() {
+        return this.data.split(this.key).join("");
+    }
+
+
+}
+
